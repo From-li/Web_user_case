f2345e665c8c5ee48f526afb00d42b51ffb5dc77
From_li <1257921260@qq.com>
2020-09-13 19:58:29 +0800

    111

---
+++ out/artifacts/day04_Cookie_Session_war_exploded/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Eric
+  Date: 2020/8/29
+  Time: 08:41
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+<%--  jsp案例--%>
+  <%  out.write("hehehahha!"); %>
+  <%! String name = "zhangsan";%>
+  <%= "nihaoma"%>
+  </body>
+</html>
---
+++ user_case/src/com/neusoft/web/servlet/DelSelectedServlet.java
@@ -0,0 +1,40 @@
+package com.neusoft.web.servlet;
+
+import com.neusoft.service.UserService;
+import com.neusoft.service.impl.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * @author Eric Lee
+ * @date 2020/9/1 08:49
+ */
+@WebServlet("/delSelectedServlet")
+public class DelSelectedServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            // 获取所有选中的  uid
+        String[] uids = req.getParameterValues("uid");
+
+        System.out.println(Arrays.toString(uids));
+        System.out.println("dsd");
+
+        UserService service = new UserServiceImpl();
+        service.deleteSelectedUser(uids);
+
+//        resp.sendRedirect(req.getContextPath()+"/userListServlet");
+        resp.sendRedirect(req.getContextPath()+"/findUserByPageServlet");
+
+    }
+}
